2f02a3a286ea3f673e85354ece2d33c0dd101b1d
JohnnnyTang <1275441282@qq.com>
2026-03-18 16:51:53 +0800

    release: update to v0.3

--- src/adminbounds/__init__.py
+++ src/adminbounds/__init__.py
@@ -3,5 +3,5 @@ from .client import AdminBoundsClient
 # Backwards-compatible alias
 GeoAdminClient = AdminBoundsClient
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = ["AdminBoundsClient", "GeoAdminClient"]
